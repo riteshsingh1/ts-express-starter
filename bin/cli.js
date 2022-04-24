ae25d9d04eed0601ccc57f0ce613876ead65c06f
Ritesh Singh <1124ritesh@gmail.com>
2022-04-24 17:36:28 +0530

    first commmit
    Signed-off-by: Ritesh Singh <1124ritesh@gmail.com>

--- bin/cli.js
+++ bin/cli.js
@@ -1,17 +1,17 @@
 #!/usr/bin/env node
 /*****************************************************************
  * Typescript Express Starter
- * 2019.12.18 ~ 🎮
- * Made By AGUMON 🦖
- * https://github.com/ljlm0402/typescript-express-starter
+ * 2022.04.24~ 🎮
+ * Made By Ritesh Singh
+ * https://github.com/riteshsingh1/ts-express-starter
  *****************************************************************/
 
- const path = require("path");
- const starter = require("../lib/starter");
- const destination = getDest(process.argv[2]);
- 
- function getDest(destFolder = "typescript-express-starter") {
-   return path.join(process.cwd(), destFolder);
- }
- 
- starter(destination);
+const path = require("path");
+const starter = require("../lib/starter");
+const destination = getDest(process.argv[2]);
+
+function getDest(destFolder = "typescript-express-starter") {
+  return path.join(process.cwd(), destFolder);
+}
+
+starter(destination);
