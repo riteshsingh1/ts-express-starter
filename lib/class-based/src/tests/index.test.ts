8656885279a9aa652dc1aaa226c654eb1490dfef
Ritesh Singh <1124ritesh@gmail.com>
2022-05-29 18:12:58 +0530

    class based added
    Signed-off-by: Ritesh Singh <1124ritesh@gmail.com>

---
+++ lib/class-based/src/tests/index.test.ts
@@ -0,0 +1,21 @@
+import request from 'supertest';
+import mongoose from 'mongoose';
+import App from '@app';
+import IndexRoute from '@routes/index.route';
+
+afterAll(async () => {
+  await new Promise<void>(resolve => setTimeout(() => resolve(), 500));
+});
+afterAll(() => {
+  mongoose.connection.close();
+});
+describe('Testing Index', () => {
+  describe('[GET] /', () => {
+    it('response statusCode 200', () => {
+      const indexRoute = new IndexRoute();
+      const app = new App([indexRoute]);
+
+      return request(app.getServer()).get(`${indexRoute.path}`).expect(200);
+    });
+  });
+});
